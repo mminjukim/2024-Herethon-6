325b9caf9c55e9c3f0034ef708863ecee80c208c
huggywuggy1289 <bangstation0714@naver.com>
2024-06-27 15:48:59 +0900

    3번째 backend

--- mzSkill/accounts/forms.py
+++ mzSkill/accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 # UserCreationForm: 기본 유저 생성 폼(pw1 pw2를 제공)
 
 class SignUpForm(UserCreationForm):
@@ -9,3 +10,8 @@ class SignUpForm(UserCreationForm):
         # AUTH_USER_MODEL이라고 settings.py에 정의한 user모델을 가져온다는 의미
         model = User
         fields = ['first_name', 'username', 'email']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['nickname', 'birthdate', 'profile_emoji']
