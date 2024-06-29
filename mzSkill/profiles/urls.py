ee94c1b4bbb9d623cd6109a22bf90bd82d10154b
Minju Kim <mnat713@naver.com>
2024-06-29 18:56:13 +0900

    러너 프로필보기, 수정

--- mzSkill/profiles/urls.py
+++ mzSkill/profiles/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from .views import *
 
 app_name = 'profiles'
 
 urlpatterns = [
+    path('learner/<int:id>/', show_learner_profile, name='show_learner_profile'),
+    path('learner/<int:id>/updateprofile/', update_learner_profile, name='update_learner_profile'),
+    path('learner/<int:id>/updatedetails/', update_learner_details, name='update_learner_details'),
+
 ]
--- mzSkill/profiles/views.py
+++ mzSkill/profiles/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse # 세션 구성에 필요
 
-# Create your views here.
+from accounts.forms import *
+
+def show_learner_profile(request, id):
+    profile = Learner.objects.get(id=id)
+    return render(request, 'show_learner_profile.html', {'profile':profile})
+
+def update_learner_profile(request, id):
+    profile = Learner.objects.get(id=id)
+    if request.method == 'POST':
+        form = WriteLearnerProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profiles:update_learner_details', id)
+    else:
+        form = WriteLearnerProfileForm(instance=profile)
+        return render(request, "update_learner_profile.html", {'form':form})
+
+def update_learner_details(request, id): # 수정
+    profile = Learner.objects.get(id=id)
+    if request.method == 'POST':
+        form = LearnerDetailsForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profiles:show_learner_profile', id) # id 위치 어디?
+    else:
+        form = LearnerDetailsForm(instance=profile)
+        return render(request, "update_learner_details.html", {'form':form})
+
