03e67b9990a75301ea27c284207a93b0e55c2508
Minju Kim <mnat713@naver.com>
2024-07-02 03:35:55 +0900

    자잘한 기능 수정, community 앱 추가

--- mzSkill/mzSkill/settings.py
+++ mzSkill/mzSkill/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'main',
     'accounts',
     'profiles',
+    
+    'community',
 ]
 
 MIDDLEWARE = [
--- mzSkill/profiles/views.py
+++ mzSkill/profiles/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 
 from accounts.forms import *
 from .forms import *
+from .models import *
+
+
+
+# 러너 프로필 보기, 수정 
 
 def show_learner_profile(request, id):
     profile = Learner.objects.get(id=id)
@@ -18,7 +23,7 @@ def update_learner_profile(request, id):
         form = WriteLearnerProfileForm(instance=profile)
         return render(request, "update_learner_profile.html", {'form':form})
 
-def update_learner_details(request, id): # 수정
+def update_learner_details(request, id):
     profile = Learner.objects.get(id=id)
     if request.method == 'POST':
         form = LearnerDetailsForm(request.POST, instance=profile)
@@ -30,6 +35,8 @@ def update_learner_details(request, id): # 수정
         return render(request, "update_learner_details.html", {'form':form})
 
 
+
+# 티쳐 프로필 보기, 수정  
 
 def show_teacher_profile(request, id):
     profile = Teacher.objects.get(id=id)
