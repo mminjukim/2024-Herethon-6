22f12e9932a38db9077d99ccf6e0f94b43976a6c
Minju Kim <mnat713@naver.com>
2024-07-05 18:11:41 +0900

    일부 html연결

--- mzSkill/main/views.py
+++ mzSkill/main/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from accounts.views import login
 from profiles.models import Learner, Teacher
 from django.core.paginator import Paginator
+from django.contrib.auth.models import User
+
 
 @login_required
 def main(request):
@@ -16,7 +18,7 @@ def main(request):
     try:
         profile = Learner.objects.get(user_id = request.user.id)
         my_teachers = profile.my_teachers.all()
-        teacher_pg = Paginator(my_teachers, 3)
+        teacher_pg = Paginator(my_teachers, 2)
         my_teachers = teacher_pg.get_page(pgnum)
         return render(request, 'learner_main.html', {'profile':profile, 'teachers':teachers, 'my_teachers':my_teachers})
     except Learner.DoesNotExist:
